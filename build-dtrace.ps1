e5c260d9e7ac7fda0b40a068a95baf7e010f7246
Cyber Phantom <inam123451@gmail.com>
2024-01-03 14:52:03 +0100

    Only download external tools if they are not already downloaded

--- build-dtrace.ps1
+++ build-dtrace.ps1
@@ -1,4 +1,11 @@
 Set-Location -Path .\target\dtrace\
-& .\releng\Get-ExternalTools.ps1
+
+$externalToolsPath = ".\releng\external"
+$externalToolsDownloaded = Test-Path $externalToolsPath
+
+if (-not $externalToolsDownloaded) {
+    & .\releng\Get-ExternalTools.ps1
+}
+
 Set-ExecutionPolicy RemoteSigned -Scope Process
 & 'C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe' .\opendtrace.sln /t:dtrace_dll:Rebuild /p:Configuration=Release /p:Platform=x64
