b76cd7bb887275b662e30614efce554f64a440e7
Cyber Phantom <inam123451@gmail.com>
2024-01-05 11:53:44 +0100

    Add dtrace_status enum and dtrace_status method
    Added  `dtrace_status` enum and wrapped `dtrace_status`. The `dtrace_status` enum represents the different status values returned by the DTrace library, and the `dtrace_status` method retrieves the current status of the DTrace instance.

--- src/wrapper.rs
+++ src/wrapper.rs
@@ -29,6 +29,33 @@ pub enum dtrace_aggwalk_order {
     ValVarRevSorted,
 }
 
+#[repr(u32)]
+pub enum dtrace_status {
+    /// No Status
+    None = crate::DTRACE_STATUS_NONE,
+    /// Status OK
+    Ok = crate::DTRACE_STATUS_OKAY,
+    /// `exit()` was called, tacing stopped
+    Exited = crate::DTRACE_STATUS_EXITED,
+    /// Fill buffer full, tracing stopped
+    Filled = crate::DTRACE_STATUS_FILLED,
+    /// Tracing already stopped
+    Stopped = crate::DTRACE_STATUS_STOPPED,
+}
+
+impl From<u32> for dtrace_status {
+    fn from(value: u32) -> Self {
+        match value {
+            crate::DTRACE_STATUS_NONE => dtrace_status::None,
+            crate::DTRACE_STATUS_OKAY => dtrace_status::Ok,
+            crate::DTRACE_STATUS_EXITED => dtrace_status::Exited,
+            crate::DTRACE_STATUS_FILLED => dtrace_status::Filled,
+            crate::DTRACE_STATUS_STOPPED => dtrace_status::Stopped,
+            _ => panic!("Invalid dtrace_status value"),
+        }
+    }
+}
+
 impl dtrace_hdl {
     /// Opens a DTrace instance with the specified version and flags.
     ///
@@ -82,6 +109,14 @@ impl dtrace_hdl {
         } else {
             Err(DtraceError::from(self.dtrace_errno()))
         }
+    }
+
+    pub fn dtrace_status(&self) -> dtrace_status {
+        let status;
+        unsafe {
+            status = crate::dtrace_status(self.handle) as u32;
+        }
+        status.into()
     }
 
     /// Sets a handler function for processing buffered trace data.
