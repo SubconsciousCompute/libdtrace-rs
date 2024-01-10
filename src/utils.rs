21a70e6cb00e2d99eb0b52e862aca1f8d6823ade
Cyber Phantom <inam123451@gmail.com>
2024-01-10 11:06:57 +0530

    Add a wrapper for `fopen`
    Fixes #4

--- src/utils.rs
+++ src/utils.rs
@@ -3,7 +3,7 @@ use crate::wrapper::dtrace_hdl;
 #[derive(Debug)]
 pub enum DtraceError {
     Errno(i32),
-} 
+}
 
 // Get message using dtrace_errmsg
 impl std::fmt::Display for DtraceError {
@@ -22,3 +22,21 @@ impl From<i32> for DtraceError {
 }
 
 impl std::error::Error for DtraceError {}
+
+extern "C" {
+    pub fn fopen(
+        __filename: *const ::core::ffi::c_char,
+        __modes: *const ::core::ffi::c_char,
+    ) -> *mut crate::FILE;
+}
+
+pub fn openf(filename: &str, modes: &str) -> Result<*mut crate::FILE, String> {
+    let filename = std::ffi::CString::new(filename).unwrap();
+    let modes = std::ffi::CString::new(modes).unwrap();
+    let file = unsafe { fopen(filename.as_ptr(), modes.as_ptr()) };
+    if file.is_null() {
+        Err("Failed to open file".to_string())
+    } else {
+        Ok(file)
+    }
+}
