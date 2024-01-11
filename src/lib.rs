3cc72b375016fe462d5516963e0e1e08519206af
Cyber Phantom <inam123451@gmail.com>
2024-01-11 11:47:00 +0530

    Simply unit tests

--- src/lib.rs
+++ src/lib.rs
@@ -12,62 +12,38 @@ mod tests {
     use crate::*;
     use wrapper::dtrace_hdl;
     #[test]
-    fn dtrace_get_handle() {
-        let handle = dtrace_hdl::dtrace_open(DTRACE_VERSION as i32, 0);
-        match handle {
-            Ok(_) => {}
-            Err(error) => {
-                panic!("{}", error);
-            }
-        }
+    fn dtrace_get_handle() -> Result<(), utils::Error> {
+        dtrace_hdl::dtrace_open(DTRACE_VERSION as i32, 0)?;
+        Ok(())
     }
 
     #[test]
-    fn dtrace_set_option() {
-        let handle = dtrace_hdl::dtrace_open(DTRACE_VERSION as i32, 0).unwrap();
-        let status = handle.dtrace_setopt("bufsize", "4m");
-        match status {
-            Ok(_) => {}
-            Err(error) => {
-                panic!("{}", error);
-            }
-        }
+    fn dtrace_set_option() -> Result<(), utils::Error> {
+        let result = dtrace_hdl::dtrace_open(DTRACE_VERSION as i32, 0)?
+                    .dtrace_setopt("bufsize", "4m")?;
+        assert_eq!(result.dtrace_getopt("bufsize")?, 4194304);
+        Ok(())
     }
 
     #[test]
-    fn dtrace_handle_buffered() {
-        let handle = dtrace_hdl::dtrace_open(DTRACE_VERSION as i32, 0).unwrap();
-        let status = handle.dtrace_register_handler(crate::types::dtrace_handler::Buffered(Some(callbacks::buffered)), None);
-        match status {
-            Ok(_) => {}
-            Err(error) => {
-                panic!("{}", error);
-            }
-        }
+    fn dtrace_handle_buffered() -> Result<(), utils::Error> {
+        dtrace_hdl::dtrace_open(DTRACE_VERSION as i32, 0)?
+            .dtrace_register_handler(crate::types::dtrace_handler::Buffered(Some(callbacks::buffered)), None)?;
+        Ok(())
     }
 
     #[test]
-    fn dtrace_compile_and_exec() {
-        let handle = dtrace_hdl::dtrace_open(DTRACE_VERSION as i32, 0).unwrap();
+    fn dtrace_compile_and_exec() -> Result<(), utils::Error> {
+        let handle = dtrace_hdl::dtrace_open(DTRACE_VERSION as i32, 0)?;
         let prog = handle
                     .dtrace_program_strcompile(
                         "dtrace:::BEGIN {trace(\"Hello World\");} syscall:::entry { @num[execname] = count(); }", 
                         dtrace_probespec::DTRACE_PROBESPEC_NAME, 
                         DTRACE_C_ZDEFS,
-                        None);
-        match prog {
-            Ok(prog) => {
-                let status = handle.dtrace_program_exec(prog, None);
-                match status {
-                    Ok(_) => {}
-                    Err(error) => {
-                        panic!("{}", error);
-                    }
-                }
-            }
-            Err(error) => {
-                panic!("{}", error);
-            }
-        }                        
+                        None)?;
+        
+        handle.dtrace_program_exec(prog, None)?;
+
+        Ok(())
     }
 }
